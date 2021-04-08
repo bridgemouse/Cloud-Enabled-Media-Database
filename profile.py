b594e7aa4b8892690e11ef0c53047dec8acd4297
aylingc <37917054+aylingc@users.noreply.github.com>
2021-04-08 17:54:21 -0400

    Update profile.py
    Change profile.py to experiment with LAPD

--- profile.py
+++ profile.py
@@ -9,6 +9,10 @@ request = pc.makeRequestRSpec()
 tourDescription = \
 """
 This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
+-------------------------------------------------------------------------------------------
+SSO Edit:
+This profile provides a two-node set to study SSO. One node will be the LDAP server, and the other
+node is a client who is to be authenticated using LDAP.
 """
 
 #
@@ -21,19 +25,17 @@ request.addTour(tour)
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-for i in range(3):
+for i in range(2):
   if i == 0:
-    node = request.XenVM("head")
+    node = request.XenVM("ldapserver")
   else:
-    node = request.XenVM("worker-" + str(i))
-  node.cores = 4
-  node.ram = 8192
+    node = request.XenVM("ldapclient")
+
   node.routable_control_ip = "true" 
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
-  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
   
 pc.printRequestRSpec(request)
